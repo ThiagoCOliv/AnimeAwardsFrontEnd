561a9fc62606ab56c2d4359b41e5ec92ddb42614
Thiago Correa <thiagocdo445@gmail.com>
2025-01-14 16:31:43 -0300

    Mudança de botão para o componente btnAdd e correção da requisição ao backend ao adicionar indicado

--- src/app/pages/categoria/categoria.component.ts
+++ src/app/pages/categoria/categoria.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CategoriaService } from '../../services/categoria.service';
+import { IndicadoService } from '../../services/indicado.service';
 import { Categoria } from '../../interfaces/categoria.interface';
 import { Indicado } from '../../interfaces/indicado.interface';
 import { ModalIndicadoComponent } from "../../components/modal-indicado/modal-indicado.component";
+import { BtnAddComponent } from "../../components/btn-add/btn-add.component";
 
 @Component({
   selector: 'app-categoria',
   standalone: true,
-  imports: [ModalIndicadoComponent],
+  imports: [ModalIndicadoComponent, BtnAddComponent],
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.scss'
 })
@@ -28,10 +30,10 @@ export class CategoriaComponent {
     this.nomeCategoria = categoriaNome;
   }
 
-  constructor(private service: CategoriaService){}
+  constructor(private catService: CategoriaService, private indService: IndicadoService){}
 
   ngOnInit(): void {
-    this.service.getCategory(this.nomeCategoria).subscribe(res => {
+    this.catService.getCategory(this.nomeCategoria).subscribe(res => {
       this.categoria = res;
       this.indicados = [...res.indicados];
       this.podeAddManualmente = this.podeAddIndicado(this.categoria);
@@ -49,27 +51,7 @@ export class CategoriaComponent {
   }
 
   onEditar(){
-    this.service.putCategory(this.categoria, this.indicados).subscribe(res => {
-      this.mensagem = res.body.mensagem;
-
-      if(res.status == 201)
-      {
-        this.statusRequisicao = "sucesso"
-        this.categoria.indicados = [...this.indicados];
-        setTimeout(() => {
-          this.alteracao = false;
-          this.mensagem = "";
-          this.statusRequisicao = ""
-        }, 3000)
-      }
-      else{
-        this.statusRequisicao = "erro"
-        setTimeout(() => {
-          this.mensagem = ""
-          this.statusRequisicao = ""
-        }, 3000)
-      }
-    })
+    this.catService.putCategory(this.categoria, this.indicados).subscribe(res => this.exibirRespostaReq(res))
   }
 
   atualizarIndicados(ascendidoIndex: number, rebaixadoIndex: number):Indicado[]
@@ -110,17 +92,19 @@ export class CategoriaComponent {
 
   addIndicado(indicado: Indicado | string)
   {
-    this.modalAberto = false;
-    
     if(indicado == '')
     {
       alert('Não é possível adicionar novos indicados!');
     }
     else if(indicado != 'cancel' && indicado)
     {
-      this.indicados.push(indicado as Indicado);
-      this.onEditar();
+      this.indService.postIndicado(indicado as Indicado, this.categoria).subscribe(res => {
+        res.status == 201 ? this.indicados.push(indicado as Indicado) : ""
+        this.exibirRespostaReq(res)
+      })
     }
+
+    this.modalAberto = false;
   }
 
   podeAddIndicado(categoria: Categoria): boolean
@@ -128,5 +112,28 @@ export class CategoriaComponent {
     if(categoria.tipo == "Personagens"){ return true; }
     if(categoria.tipo == "Subjetivas" && (categoria.nome == "Melhor Encerramento" || categoria.nome == "Melhor Abertura")){ return true; }
     return false;
+  }
+
+  exibirRespostaReq(res: any)
+  {
+    this.mensagem = res.body.mensagem;
+
+    if(res.status == 201)
+    {
+      this.statusRequisicao = "sucesso"
+      this.categoria.indicados = [...this.indicados];
+      setTimeout(() => {
+        this.alteracao = false;
+        this.mensagem = "";
+        this.statusRequisicao = ""
+      }, 3000)
+    }
+    else{
+      this.statusRequisicao = "erro"
+      setTimeout(() => {
+        this.mensagem = ""
+        this.statusRequisicao = ""
+      }, 3000)
+    }
   }
 }
