bcbad8914c063103f4e5108bee59511e8740881c
Thiago Correa <thiagocdo445@gmail.com>
2025-01-14 16:23:57 -0300

    Refatoração, alteração no css e adição para destacar a pagina atual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,6 +9,45 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+  paginas = [
+    {
+      titulo: "Home",
+      status: ""
+    },
+    {
+      titulo: "Animes",
+      status: ""
+    },
+    {
+      titulo: "Categorias",
+      status: ""
+    }
+  ];
+
+  ngOnInit(): void {
+    let paginaAtual = sessionStorage.getItem("pagina");
+    
+    if(paginaAtual)
+    {
+      let numeroPagina = parseInt(paginaAtual);
+      this.paginas.forEach((pag, indice) => pag.status = indice == numeroPagina ? "ativa" : "inativa");
+    }
+    else
+    {
+      this.paginas[0].status = "ativa";
+      this.paginas[1].status = "inativa";
+      this.paginas[2].status = "inativa";
+    }
+  }
+
+  alterarPagina(pagina: number)
+  {
+    if(pagina < this.paginas.length)
+    {
+      this.paginas.forEach((pag, indice) => pag.status = indice == pagina ? "ativa" : "inativa");
+      sessionStorage.setItem("pagina", pagina.toString())
+    }
+  }
   title = 'anime-awards';
 }
